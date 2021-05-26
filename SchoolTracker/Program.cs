becae9c3c2bc77640f5494d2951388a71b834892
MANIKARNIKA <manikarnikadixit@gmail.com>
2021-05-26 15:49:13 +0530

    Initial commit

---
+++ SchoolTracker/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SchoolTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new List<Student>();
+
+            var adding = true;
+
+            while (adding)
+            {
+                var newStudent = new Student();
+
+                newStudent.Name = Util.C onsole.Ask("Student Name: ");
+                
+                 var result = int.TryParse(Util.Console.Ask("Student Grade: "),out newStudent.Grade);
+
+                 if (!result)
+                 {
+                     Console.WriteLine("Error , please enter a number");
+                 }
+
+                newStudent.Birthday = Util.Console.Ask("Student Birthday: ");
+
+                newStudent.Address = Util.Console.Ask("Student Address: ");
+
+                newStudent.Phone = int.Parse(Util.Console.Ask("Student Phone Number: "));
+
+                students.Add(newStudent);
+                Student.Count++;
+                Console.WriteLine("Student Count: {0}", Student.Count);
+
+                Console.WriteLine("Add another? y/n");
+
+                if (Console.ReadLine() != "y")
+                    adding = false;
+            }
+
+            foreach (var student in students)
+            {
+                Console.WriteLine("Name: {0}, Grade: {1}", student.Name, student.Grade);
+            }
+        }
+    }
+
+    class Student
+    {
+        static public int Count = 0;
+
+        public string Name;
+        public int Grade;
+        public string Birthday;
+        public string Address;
+        private int phone;
+
+        public int Phone
+        {
+            set { phone = value; }
+        }
+
+        public void SetPhone(int number)
+        {
+            phone = number;
+        }
+    }
+}
